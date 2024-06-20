685b6ca74bc5ca2ea3137969da374c2f7413f68b
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2024-06-20 14:34:00 +0200

    example for columns detection

--- examples/main_col_detection.py
+++ examples/main_col_detection.py
@@ -1,10 +1,11 @@
 import os
 import csv_pm_llm_parsing
 import traceback
+import pandas as pd
 
 
 def execute_script():
-    path = "../testfiles/sep_detection"
+    path = "../testfiles/cid_acti_timest"
 
     files = os.listdir(path)
 
@@ -13,12 +14,12 @@ def execute_script():
             full_path = os.path.join(path, file)
 
             try:
-                dataframe = csv_pm_llm_parsing.detect_sep_and_load(full_path, input_encoding="utf-8")
+                dataframe = csv_pm_llm_parsing.detect_caseid_activity_timestamp(pd.read_csv(full_path, sep=',', quotechar='\"', encoding="utf-8"))
                 dataframe.info()
                 print(dataframe)
             except:
                 traceback.print_exc()
-                print("failed detection for", full_path)
+                print("failed column detection for", full_path)
 
 
 if __name__ == "__main__":
